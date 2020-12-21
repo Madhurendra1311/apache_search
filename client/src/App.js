d08936ac42e248cde2c4343ea498da33689be8ac
Madhurendra1311 <kmadhu1311@gmail.com>
2020-12-21 23:24:44 +0530

    added some file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom"
+import ShowStudent from "./Component/ShowStudent"
+import AddStudent from "./Component/AddStudent"
+import EditStudent from "./Component/EditStudent"
+import Register from "./Component/Register"
+import Login from "./Component/Login"
+import Home from "./Component/Home"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function isTokenValid(){
+  let token = window.localStorage.getItem('token')
+  if(token !== null){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+          <Route path="/showstudent" render={() => isTokenValid() ? <ShowStudent />: <Redirect to="/"/>} />
+          <Route path="/addStudent" render={() =>  isTokenValid() ?  <AddStudent />: <Redirect to="/"/>} />
+          <Route path="/editStudent" render={() =>  isTokenValid() ?  <EditStudent />: <Redirect to="/"/>} />
+          <Route path="/register" render={()=> <Register />} />
+          <Route path="/login" render={()=> <Login />} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
+}
